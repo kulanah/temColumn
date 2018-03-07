8b90f5b81f0dd77b8c3930f0230fcc9d14b3f733
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-03-07 14:19:20 -0800

    added default lens creation

---
+++ public/js/SimpleComponent.js
@@ -0,0 +1,27 @@
+class SimpleComponent{
+  constructor(){
+
+  }
+  constructor(f, y, lH){
+    this.focalLength = f;
+    this.yOffset = y;
+    this.lensHeight = lH + y;
+  }
+
+  drawLens(){
+    this.lensShape = new THREE.SphereGeometry(4, 16, 12);
+    this.lensShape.applyMatrix(new THREE.Matrix4().makeScale(1, 0.5, 3));
+    this.lensMat = new THREE.MeshBasicMaterial({color: 0x00000, wireframe: true});
+
+    this.lensMesh = new THREE.Mesh(this.lensShape, this.lensMat);
+
+    scene.add(this.lensMesh);
+  }
+
+
+  draw(){
+    drawLens();
+  }
+
+
+}
