eab8375e01a63cb04e6339b8bbb7f31de89d1089
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-03-07 14:02:57 -0800

    rewrote default column to draw column and that's all

--- public/js/column.js
+++ public/js/column.js
@@ -1,18 +1,18 @@
 class column{
-  constructor(){
+  constructor(columnHeight){
     this.components = new Array();
+    this.columnHeight = columnHeight;
+
   }
 
-  addLens(originSide, height, centerPoint, y, focalLength){
-    let newComp = new component(originSide, height, centerPoint, y, focalLength);
-    this.components.push(newComp);
-  }
+  drawColumn(){
+    //radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength
+    this.shape = new THREE.CylinderGeometry(5, 5, this.columnHeight, 8, 1, true, 1.6, Math.PI);
+    this.mats = new THREE.MeshBasicMaterial({color: 0xff69b4, side: THREE.DoubleSide  });
 
-  drawLenses(scene, renderer, camera){
-    this.components.map((current) => {
-      current.drawLens(scene);
-    });
+    this.mesh = new THREE.Mesh(this.shape, this.mats);
+    this.mesh.position.y = -this.columnHeight / 2;
 
-    renderer.render(scene, camera);
+    scene.add(this.mesh);
   }
-}
+};
