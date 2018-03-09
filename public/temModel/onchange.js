2919501813ca540b547d96f00ccb6f53099c619c
kulanah <devonthebutler@gmail.com>
2018-03-09 13:26:59 -0800

    added onchange for second component

--- public/temModel/onchange.js
+++ public/temModel/onchange.js
@@ -1,3 +1,6 @@
 $('#focalslider').on('input', function(){
   temColumn.components[0].updateFocalLength(this.value);
 });
+$('#focalslider2').on('input', function(){
+  temColumn.components[1].updateFocalLength(this.value);
+});
