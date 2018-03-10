537ddfa31de6eeced6614f56c06a379bc4543d5e
kulanah <devonthebutler@gmail.com>
2018-03-09 17:29:09 -0800

    updated tem column to user column

--- public/temModel/onchange.js
+++ public/temModel/onchange.js
@@ -1,6 +1,6 @@
 $('#focalslider').on('input', function(){
-  temColumn.components[0].updateFocalLength(this.value);
+  userColumn.components[0].updateFocalLength(this.value);
 });
 $('#focalslider2').on('input', function(){
-  temColumn.components[1].updateFocalLength(this.value);
+  userColumn.components[1].updateFocalLength(this.value);
 });
