b4aadc97757486e7ba0eed9f2809a9b9193ef789
kulanah <devonthebutler@gmail.com>
2018-03-12 16:38:55 -0700

    made onchange functions for the new inputs

--- public/temModel/onchange.js
+++ public/temModel/onchange.js
@@ -1,6 +1,15 @@
 $('#focalslider').on('input', function(){
   userColumn.components[0].updateFocalLength(this.value);
 });
+
 $('#focalslider2').on('input', function(){
   userColumn.components[1].updateFocalLength(this.value);
+});
+
+$('#x1').on('input', function(){
+  userColumn.components[1].updatex1(this.value);
+});
+
+$('#x2').on('input', function(){
+  userColumn.components[1].updatex2(this.value);
 });
