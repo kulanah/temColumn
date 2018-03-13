ab09eb803843337ef638f2e4272fcc23b242946a
kulanah <devonthebutler@gmail.com>
2018-03-13 13:34:04 -0700

    reorganized files in prep for public launch

--- public/js/Column.js
+++ public/js/Column.js
@@ -1,26 +1,62 @@
 class Column{
-  constructor(columnHeight, lensWidth){
+  constructor(height ){
     this.components = new Array();
-    this.columnHeight = columnHeight;
-    this.lensOffset = 0;
-    this.lensWidth = lensWidth;
+    this.height = height;
+    //radius top, radius bottom, height, radial segments, height segments, open ended, theta start, theta length
+    this.cMaterial = new THREE.CylinderGeometry(10, 10, this.height, 20, 1, true, 1.5, 3);
+    this.cGeometry = new THREE.MeshPhongMaterial({
+    // this.cGeometry = new THREE.MeshBasicMaterial({
+      side:  THREE.FrontSide, 
+      shadowSide: THREE.BackSide,
+      wireframe: false, 
+      color: 0xffffff
+    });
 
 
+    this.width = 2.5; 
+
+    this.lineMaterial = new THREE.LineBasicMaterial({color: 0xff0000, side: THREE.DoubleSide});
+    this.lineGeometry = new THREE.Geometry();
+
+    // this.cGeometry = new THREE.MeshBasicMaterial({
+    //   side:  THREE.DoubleSide, 
+    //   color: 0xaaaaaa,
+    // });
+
+    this.cMaterial.computeFaceNormals();
+    this.cMaterial.computeVertexNormals();
+
+    this.cMesh = new THREE.Mesh(this.cMaterial, this.cGeometry);
+
+    this.cMesh.position.y = this.height / 2;
+
+    this.init = this.init.bind(this);
   }
 
-  drawColumn(){
-    //radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength
-    this.shape = new THREE.CylinderGeometry(5, 5, this.columnHeight, 8, 1, true, 1.6, Math.PI);
-    this.mats = new THREE.MeshBasicMaterial({color: 0x333333, side: THREE.DoubleSide  });
-
-    this.mesh = new THREE.Mesh(this.shape, this.mats);
-    this.mesh.position.y = -this.columnHeight / 2;
-
-    scene.add(this.mesh);
+  addSimpleLens(focal, lensHeight){
+    if (this.components.length == 0){
+      let newComp = new SimpleLens(focal, 0, this.width, 0, lensHeight);
+      this.components.push(newComp);
+      // this.drawLenses();
+    } else {
+      let startY = this.components[this.components.length - 1].getHeight();
+      let newComp = new SimpleLens(focal, startY, this.width, 0, lensHeight);
+      this.components.push(newComp);
+      // this.drawLenses();
+    }
   }
-  
-  addLens(focalLength, lensHeight, lensWidth){
-    this.components.push(new SimpleComponent(focalLength, this.lensOffset, lensHeight, lensWidth));
+
+  addAngledLens(focal, lensHeight, x1, x2){
+    if (this.components.length == 0){
+      let newComp = new AngledLens(focal, 0, this.width, 0, lensHeight, x1, x2);
+      this.components.push(newComp);
+      // this.drawLenses();
+    } else {
+      let startY = this.components[this.components.length - 1].getHeight();
+      let newComp = new AngledLens(focal, startY, this.width, 0, lensHeight, x1, x2);
+      this.components.push(newComp);
+      // this.drawLenses();
+    }
   }
 
   drawLenses(){
@@ -28,4 +64,21 @@ class Column{
       this.components[i].draw();
     }
   }
-};
+
+  drawColumn(){
+    scene.add(this.cMesh);
+
+    this.lineGeometry.vertices.push(new THREE.Vector3(0,0,0));
+    this.lineGeometry.vertices.push(new THREE.Vector3(0,-45 ,0));
+
+    this.line = new THREE.Line(this.lineGeometry, this.lineMaterial);
+
+    scene.add(this.line);
+  }
+
+
+  init(){
+    this.drawColumn();
+  }
+
+}
--- public/js/app.js
+++ public/js/app.js
@@ -1,168 +1,113 @@
-'use strict';
-
-let renderer, scene, camera, controls;
-
-//this sets the column height and gives us a camera offset
-let columnHeight = 40;
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.1, 1500);
+let controls;
+let renderer;
+let columnHeight = -45;
 
 let init = function(){
-  initRenderer();
-  initLight();
-  initCamera();
-  initControls();
-  initPointSphere();
-}
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.domElement.id = 'threeCanvas';
+  document.body.appendChild(renderer.domElement);
 
-let initRenderer = function(){
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.domElement.id = 'threejscanvas';
-    document.getElementById('viewport').appendChild(renderer.domElement);
+  controls = new THREE.TrackballControls(camera, document.getElementById('threeCanvas'));
+  controls.addEventListener('change', render);
 
-    scene = new THREE.Scene();
-}
 
-let initLight = function(){
-  let keyLight = new THREE.DirectionalLight(0xff0000, 1, 0);
-  keyLight.position.set(4,5,1);
+  let xVal = -3;
+  // camera.positon.set(0,0,0);
+  // camera.positon.set(new THREE.Vector3(0,0,0));
+  camera.position.x = 0;
+  camera.position.z = 200;
+  camera.position.y = 0;
+  camera.position.x = xVal;
+
+  controls.target = new THREE.Vector3(xVal, columnHeight / 2, 0);
+  initLights();
+
+  createLenses();
+
+};
+
+
+let initLights = function() {
+  let xVal = -5;
+  let yVal = -20;
+  let zVal = 10;
+
+  let geo = new THREE.SphereGeometry(.25, 3, 3);
+  let mat = new THREE.MeshBasicMaterial({wireframe: true, color: 0xff69b4});
+  let sphere = new THREE.Mesh(geo, mat);
+  sphere.position.x = xVal;
+  sphere.position.y = yVal;
+  sphere.position.z = zVal;
+
+  scene.add(sphere);
+
+  // let keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(0, 100%, 100%)'), 1);
+  let keyLight = new THREE.PointLight(new THREE.Color('hsl(0, 100%, 100%)'), 1.4);
+  keyLight.position.set(0, 0, -140);
+  // keyLight.target.position.set = (0,-10,0);
   
-  let backLight = new THREE.DirectionalLight(0x00ffff,  1, 0);
-  backLight.position.set(-7, 3, -5);
+  let fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(285, 100%, 100%)'), 1);
+  fillLight.position.set(xVal, yVal, zVal).normalize();
+  fillLight.target.position.set(0,-20,0);
+  
+  let backLight = new THREE.DirectionalLight(new THREE.Color('hsl(58, 100%, 100%)'), 1);
+  backLight.position.set(0,0,50);
+  
+  let topLight = new THREE.DirectionalLight(new THREE.Color('hsl(338, 100%, 100%)'), 1);
+  topLight.position.set(0, 10, 0).normalize();
 
-  let underLight = new THREE.DirectionalLight(0xffffff, 1, 0);
-  underLight.position.set(0,-5,0);
-
-  scene.add(backLight);
-  scene.add(keyLight);
-  scene.add(underLight);
-}
-
-let initCamera = function(){
-  camera = new THREE.PerspectiveCamera(
-    35,                                       //fov
-    window.innerWidth / window.innerHeight,   //aspect ratio  
-    0.5,                                      //near plane 
-    1000                                      //far plane
-  );
-
-  // camera.position.set(0, -columnHeight / 2,80);
-  // camera.position.set(0, -columnHeight / 8, 30);
-  camera.position.set(5, 10, 35);
-  // camera.position.set(0,0,20);
-  scene.add(camera);
-}
-
-let initControls = function(){
-  controls = new THREE.TrackballControls(camera, document.getElementById('threejscanvas'));
-  controls.addEventListener('change', render)
-  // controls.target.set(0, 19, 29 );
-  // controls.target.set(0, -columnHeight / 8, 0);
-  // controls.target.set(0, -columnHeight / 2, 0);
-}
+  scene.add(fillLight);
+  // scene.add(keyLight);
+  // scene.add(backLight);
+  // scene.add(topLight);
+};
 
 let render = function(){
   renderer.render(scene, camera);
-}
+};
+
+let drawScene = function(){
+  userColumn.init();
+  render();
+};
 
 let animate = function(){
   requestAnimationFrame(animate);
   controls.update();
-  render();
+};
+
+let createLenses = function(){
+  userColumn.addSimpleLens(1.2, 8);
+  // userColumn.addSimpleLens(2, 5);
+  userColumn.addAngledLens(2, 5, -2, -1);
+
+
+  userColumn.drawLenses(); 
+
+
+};
+
+
+let userColumn = new Column(columnHeight);
+
+
+let draggables = [];
+
+for (let i = 0; i < draggables.length; ++i){
+  $('#' + draggables[i]).draggable({
+    addClasses: true,
+    // cancel: 'map, iframe',
+    // iframeFix: true,
+    cursor: 'move'
+  });
 }
 
-let createShapes = function(){
-  let coneGeo = new THREE.ConeGeometry(2, 5, 100, 1);
-  let coneMats = new THREE.MeshBasicMaterial({color: 0xff69b4, wireframe: true});
-
-  let cone = new THREE.Mesh(coneGeo, coneMats);
-  cone.rotation.z = 0.4
-
-  let coneBSP = new ThreeBSP(cone);
+$('.hidden').hide();
 
 
-  let cubeGeo = new THREE.BoxGeometry(12,10,10);
-  let cubeMats = new THREE.MeshBasicMaterial({color: 0x00ffff, wireframe: true});
-
-  let cube = new THREE.Mesh(cubeGeo, cubeMats);
-  cube.position.y -= 5;
-
-  let cubeBSP = new ThreeBSP(cube);
-
-  let subBSP = coneBSP.subtract(cubeBSP);
-  let result = subBSP.toMesh(new THREE.MeshBasicMaterial({transparent: false, color: 0xffffff, wireframe: true, wireframeLinejoin: "miter", wireframeLinewidth: 10})); 
-
-  // result.geometry.computeVertexNormals();
-  scene.add(result);
-
-  // scene.add(cube);
-  // scene.add(cone);
-}
-
-let initPointSphere = function(){
-  let geo = new THREE.SphereGeometry(.25,.25,1,1);
-  let mats = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffffff});
-
-  let shape = new THREE.Mesh(geo, mats);
-  shape.position.x = 4;
-  shape.position.y = 4.5;
-  shape.position.z = 0;
-
-  scene.add(shape);
-
-
-
-  let cGeo = new THREE.ConeGeometry(4,9,9);
-  let cShape = new THREE.Mesh(cGeo, mats);
-
-  cShape.rotation.z = 2
-  cShape.position.x = 4;
-  cShape.position.y = 4.5;
-  scene.add(cShape);
-}
-
-
-let temColumn;
-let columnInit = function(){
-
-  temColumn = new Column(columnHeight);
-
-  temColumn.addLens(1, 3, 4);
-
-  temColumn.drawColumn();
-  temColumn.drawLenses();
-
-}
-
-
-let initFacing = function(){
-  var material = new THREE.MeshBasicMaterial( { color : 0x00cc00 } );
-
-  //create a triangular geometry
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push( new THREE.Vector3( -5, -5, 0 ) );
-  geometry.vertices.push( new THREE.Vector3(  5, -5, 0 ) );
-  geometry.vertices.push( new THREE.Vector3(  5,  5, 0 ) );
-
-  //create a new face using vertices 0, 1, 2
-  var normal = new THREE.Vector3( 0, 1, 0 ); //optional
-  var color = new THREE.Color( 0xffaa00 ); //optional
-  var materialIndex = 0; //optional
-  var face = new THREE.Face3( 0, 1, 2);
-
-  //add the face to the geometry's faces array
-  geometry.faces.push( face );
-
-  //the face normals and vertex normals can be calculated automatically if not supplied above
-  geometry.computeFaceNormals();
-  geometry.computeVertexNormals();
-
-  scene.add( new THREE.Mesh( geometry, material));
-}
 init();
-// createShapes();
 animate();
-render();
-// columnInit();
-
-// initFacing();
-render();
+drawScene();
