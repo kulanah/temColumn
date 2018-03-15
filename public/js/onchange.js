6c407ea287b851f427c802856e70633ab02e49c7
kulanah <devonthebutler@gmail.com>
2018-03-15 15:49:06 -0700

    updated boxes to use public api for lenses

--- public/js/onchange.js
+++ public/js/onchange.js
@@ -1,15 +1,15 @@
 $('#focalslider').on('input', function(){
-  userColumn.components[0].updateFocalLength(this.value);
+  column.updateFocalLength(0, this.value);
 });
 
 $('#focalslider2').on('input', function(){
-  userColumn.components[1].updateFocalLength(this.value);
+  column.updateFocalLength(1, this.value);
 });
 
 $('#x1').on('input', function(){
-  userColumn.components[1].updatex1(this.value);
+  column.updateLeftBoundry(1, this.value);
 });
 
 $('#x2').on('input', function(){
-  userColumn.components[1].updatex2(this.value);
+  column.updateRightBoundry(1, this.value);
 });
