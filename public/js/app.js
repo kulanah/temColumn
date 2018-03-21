1c96101d2a178483a9cb847452901901903f582a
kulanah <devonthebutler@gmail.com>
2018-03-21 12:57:55 -0700

    removed extra test code

--- public/js/app.js
+++ public/js/app.js
@@ -31,8 +31,6 @@ class MicroscopeColumn {
     this.controls = new THREE.TrackballControls(this.camera, document.getElementById('threeCanvas'));
 
     let xVal = -3;
-    // camera.positon.set(0,0,0);
-    // camera.positon.set(new THREE.Vector3(0,0,0));
     this.camera.position.x = 0;
     this.camera.position.z = 200;
     this.camera.position.y = 0;
@@ -48,15 +46,6 @@ class MicroscopeColumn {
     let xVal = -5;
     let yVal = -20;
     let zVal = 10;
-
-    let geo = new THREE.SphereGeometry(.25, 3, 3);
-    let mat = new THREE.MeshBasicMaterial({wireframe: true, color: 0xff69b4});
-    let sphere = new THREE.Mesh(geo, mat);
-    sphere.position.x = xVal;
-    sphere.position.y = yVal;
-    sphere.position.z = zVal;
-
-    this.scene.add(sphere);
 
     // let keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(0, 100%, 100%)'), 1);
     let keyLight = new THREE.PointLight(new THREE.Color('hsl(0, 100%, 100%)'), 1.4);
@@ -74,9 +63,6 @@ class MicroscopeColumn {
     topLight.position.set(0, 10, 0).normalize();
 
     this.scene.add(fillLight);
-    // this.scene.add(keyLight);
-    // this.scene.add(backLight);
-    // this.scene.add(topLight);
   }
 
   initColumn(){
@@ -85,7 +71,6 @@ class MicroscopeColumn {
   }
 
   render(){ 
-    // console.trace();
     this.renderer.render(this.scene, this.camera);
   }
 
