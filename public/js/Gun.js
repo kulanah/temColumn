60bfd87686cb3a5b9b717b7a3979e49188989a39
kulanah <devonthebutler@gmail.com>
2018-03-23 15:35:23 -0700

    fixed scaling weirdness

--- public/js/Gun.js
+++ public/js/Gun.js
@@ -2,7 +2,7 @@ class Gun extends ColumnComponent{
   constructor(width, scene, title){
     super(0.8, width, scene, title);
     this.height = 3;
-    this.geometry = new THREE.ConeGeometry(this.width, this.height, 5, 1);
+    this.geometry = new THREE.ConeGeometry(this.width, this.height, 4, 1);
     this.material = new THREE.MeshPhongMaterial({
       color: 0xff0000,
       // wireframe: true,
@@ -12,6 +12,9 @@ class Gun extends ColumnComponent{
 
   drawGun(){
     this.mesh = new THREE.Mesh(this.geometry, this.material);
+    // this.geometry.makeTranslation({x: 1, y: 1, z: 0.6});
+    this.geometry.applyMatrix(new THREE.Matrix4().makeScale(1, 1, 0.6));
+    
     this.mesh.position.y = this.startY;
     this.scene.add(this.mesh);
 
