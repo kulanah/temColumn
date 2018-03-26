53bd09b05c3eeef363e86638dad3aa5e18fd9b21
kulanah <devonthebutler@gmail.com>
2018-03-26 12:59:45 -0700

    added getendy as abstract function

--- public/js/ColumnComponent.js
+++ public/js/ColumnComponent.js
@@ -1,15 +1,20 @@
 class ColumnComponent{
-  constructor(startY, width, scene, title){
+  constructor(startY, width, scene, title, endY){
 
     this.startY = startY;
     this.width = width;
     this.scene = scene;
     this.title = title;
+    this.endY = endY;
   }
 
 
   getStartY(){
     return this.startY;
+  }
+
+  getEndY(){
+    throw new Error('Must implement get end method, ColumnComponent is an abstract base class');
   }
 
   draw(){
