980997c3c95b8e1abdd73cc4e027218f3d64b335
kulanah <devonthebutler@gmail.com>
2018-03-26 17:57:07 -0700

    re oved extraneous onclicks

--- public/js/onchange.js
+++ public/js/onchange.js
@@ -1,15 +1,7 @@
 $('#focalslider').on('input', function(){
-  column.updateFocalLength(1, this.value);
-});
-
-$('#focalslider2').on('input', function(){
   column.updateFocalLength(2, this.value);
 });
 
-$('#x1').on('input', function(){
-  column.updateLeftBoundry(1, this.value);
-});
-
-$('#x2').on('input', function(){
-  column.updateRightBoundry(1, this.value);
+$('#focalslider2').on('input', function(){
+  column.updateFocalLength(3, this.value);
 });
