abd6c02de4851d80329a78b6dee0e8f5e11df84a
kulanah <devonthebutler@gmail.com>
2018-03-27 13:31:25 -0700

    added addlabel support to column component

--- public/js/ColumnComponent.js
+++ public/js/ColumnComponent.js
@@ -6,6 +6,16 @@ class ColumnComponent{
     this.scene = scene;
     this.title = title;
     this.endY = endY;
+
+    this.labels = new Array();
+  }
+
+  addLabel(percentage){
+    let middle = (this.endY - this.startY / 2);
+    middle *= percentage;
+    let yPos = middle + this.startY;
+
+    this.labels.push(new Label(this.width, 0.25, this.scene, yPos));
   }
 
 
@@ -25,12 +35,17 @@ class ColumnComponent{
     throw new Error('Must implement get end method, ColumnComponent is an abstract base class');
   }
 
-  addLabel(){
-    throw new Error('Must implement add label method, ColumnComponent is an abstract base class');
-  }
 
   draw(){
-    throw new Error('Must implement draw method, ColumnComponent is an abstract base class');
+    for (label in this.labels){
+      this.scene.add(label);
+    }
+  }
+
+  clear(){
+    for (label in this.labels){
+      this.scene.remove(label);
+    }
   }
   
 }
