bf6ed9f409ca7b64a3e9d8ed51b8086cbd9f24f3
kulanah <devonthebutler@gmail.com>
2018-03-29 17:44:28 -0700

    added scaffolding for screen class

---
+++ public/js/Screen.js
@@ -0,0 +1,16 @@
+
+'use strict';
+class Screen extends ColumnComponent{
+  constructor(startY, width, scene, title){
+    super(startY, width, scene, title);
+  }
+
+  draw(){
+    super.draw();
+
+  }
+
+  clear(){
+    super.clear();
+  }
+}
