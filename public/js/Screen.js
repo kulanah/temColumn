b13c936bd3a7d1bbf47b02fdca2077bb2c88deb7
kulanah <devonthebutler@gmail.com>
2018-03-29 17:47:35 -0700

    created draw function for screen

--- public/js/Screen.js
+++ public/js/Screen.js
@@ -5,8 +5,21 @@ class Screen extends ColumnComponent{
     super(startY, width, scene, title);
   }
 
+  drawScreen(){
+    let screenGeo = new THREE.BoxGeometry(this.width, 0.25, this.width);
+    let screenMat = new THREE.MeshPhongMaterial({color: 0x00ffb4});
+
+    this.screen = new THREE.Mesh(screenGeo, screenMat);
+    this.screen.position.y = this.startY + 1;
+
+    this.scene.add(this.screen);
+
+  }
+
+
   draw(){
     super.draw();
+    drawScreen();
 
   }
 
