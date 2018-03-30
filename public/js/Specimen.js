64792eaf1909dc25efee363b9a63d0378f0349d9
kulanah <devonthebutler@gmail.com>
2018-03-30 14:03:42 -0700

    updated specimen to show up on column

--- public/js/Specimen.js
+++ public/js/Specimen.js
@@ -6,16 +6,24 @@ class Specimen extends ColumnComponent{
   }
 
   drawScreen(){
-    let specimenGeo = new THREE.BoxGeometry(this.width, 0.25, this.width);
-    let specimenMat = new THREE.MeshPhongMaterial({color: 0x00ffb4});
+    let specimenGeo = new THREE.BoxGeometry(this.width * 4, 0.25, this.width * 2);
+    let specimenMat = new THREE.MeshPhongMaterial({color: 0xf48c42});
+    let frameMat = new THREE.MeshPhongMaterial({color: 0x000000, wireframe: true});
 
     this.specimen = new THREE.Mesh(specimenGeo, specimenMat);
-    this.specimen.position.y = this.startY + 1;
+    this.frameMesh = new THREE.Mesh(specimenGeo, frameMat);
+    this.specimen.position.y = -this.startY - 0.15;
+    this.specimen.position.x = -1;
+    this.frameMesh.position.y = this.specimen.position.y;
+    this.frameMesh.position.x = this.specimen.position.x;
 
     this.scene.add(this.specimen);
-
+    this.scene.add(this.frameMesh);
   }
 
+  getMiddle(){
+    return this.startY - 0.25;
+  }
 
   draw(){
     super.draw();
