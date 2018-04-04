ee7727e9b5bc0a59d26a8b1fa0c6b67931085fa3
kulanah <devonthebutler@gmail.com>
2018-04-04 12:32:38 -0700

    updated angled lens to work with single sided faces

--- public/js/AngledLens.js
+++ public/js/AngledLens.js
@@ -15,26 +15,24 @@ class AngledLens extends SimpleLens{
     //0
     rayShape1.vertices.push(new THREE.Vector3(0, - this.startY, 0));
     //1
-    rayShape1.vertices.push(new THREE.Vector3(0, 0 - this.startY, -this.width, 0));
-
-    //2
     rayShape1.vertices.push(new THREE.Vector3(newMid, -this.lensHeight - this.startY, this.depth));
-    //3
+    //2
     rayShape1.vertices.push(new THREE.Vector3(this.x1, -this.lensHeight - this.startY, 0));
-    //4
+    //3
     rayShape1.vertices.push(new THREE.Vector3(this.x2, -this.lensHeight - this.startY, 0));
-    //5
+    //4
     rayShape1.vertices.push(new THREE.Vector3(newMid, -this.lensHeight - this.startY, -this.depth));
-    //6
+    //5
     rayShape1.vertices.push(new THREE.Vector3(0, -this.lensHeight - this.startY -this.focalLength, 0));
 
-    rayShape1.faces.push(new THREE.Face3(0,2,3));
-    rayShape1.faces.push(new THREE.Face3(0,2,4));
-    rayShape1.faces.push(new THREE.Face3(0,3,5));
-    rayShape1.faces.push(new THREE.Face3(6,5,3));
-    rayShape1.faces.push(new THREE.Face3(6,2,3));
-    rayShape1.faces.push(new THREE.Face3(6,4,2));
-    rayShape1.faces.push(new THREE.Face3(6,5,6));
+    rayShape1.faces.push(new THREE.Face3(1,0,2));
+    rayShape1.faces.push(new THREE.Face3(0,1,3));
+    rayShape1.faces.push(new THREE.Face3(2,0,4));
+    rayShape1.faces.push(new THREE.Face3(4,0,3));
+    rayShape1.faces.push(new THREE.Face3(4,3,5));
+    rayShape1.faces.push(new THREE.Face3(4,5,2));
+    rayShape1.faces.push(new THREE.Face3(5,1,2));
+    rayShape1.faces.push(new THREE.Face3(5,3,1));
 
 
     rayShape1.computeFaceNormals();
@@ -45,39 +43,39 @@ class AngledLens extends SimpleLens{
     //0
     rayShape2.vertices.push(new THREE.Vector3(0, - this.startY, 0));
     //1
-    rayShape2.vertices.push(new THREE.Vector3(0, 0 - this.startY, -this.width));
-
-    //2
     rayShape2.vertices.push(new THREE.Vector3(-newMid , -this.lensHeight - this.startY, this.depth));
-    //3
+    //2
     rayShape2.vertices.push(new THREE.Vector3(-this.x1, -this.lensHeight - this.startY, 0));
-    //4
+    //3
     rayShape2.vertices.push(new THREE.Vector3(-this.x2, -this.lensHeight - this.startY, 0));
-    //5
+    //4
     rayShape2.vertices.push(new THREE.Vector3(-newMid,-this.lensHeight - this.startY, -this.depth));
-    //6
+    //5
     rayShape2.vertices.push(new THREE.Vector3(0, -this.lensHeight - this.startY -this.focalLength,0));
 
 
-    rayShape2.faces.push(new THREE.Face3(0,2,3));
+    rayShape2.faces.push(new THREE.Face3(0,1,2));
+    rayShape2.faces.push(new THREE.Face3(1,0,3));
     rayShape2.faces.push(new THREE.Face3(0,2,4));
-    rayShape2.faces.push(new THREE.Face3(0,3,5));
-    rayShape2.faces.push(new THREE.Face3(6,5,3));
-    rayShape2.faces.push(new THREE.Face3(6,2,3));
-    rayShape2.faces.push(new THREE.Face3(6,4,2));
-    rayShape2.faces.push(new THREE.Face3(6,5,4));
+    rayShape2.faces.push(new THREE.Face3(0,4,3));
+    rayShape2.faces.push(new THREE.Face3(3,4,5));
+    rayShape2.faces.push(new THREE.Face3(5,4,2));
+    rayShape2.faces.push(new THREE.Face3(1,5,2));
+    rayShape2.faces.push(new THREE.Face3(3,5,1));
 
     rayShape2.computeFaceNormals();
     rayShape2.computeVertexNormals();
 
-    this.ray2 = new THREE.Mesh(rayShape2, this.faceMat);
-
-
     this.ray= new THREE.Mesh(rayShape1, this.faceMat);
+    this.ray2 = new THREE.Mesh(rayShape2, this.faceMat);
+    this.wire1 = new THREE.Mesh(rayShape1, this.frameMat);
+    this.wire2 = new THREE.Mesh(rayShape2, this.frameMat);
 
     // this.ray.rotation.y = Math.PI / 8;
     this.scene.add(this.ray);
     this.scene.add(this.ray2);
+    this.scene.add(this.wire1);
+    this.scene.add(this.wire2);
   }
 
   updatex1(newWidth){
