b701337ce96e4bc4dd0469874b1da4b243b64930
kulanah <devonthebutler@gmail.com>
2018-04-23 12:42:57 -0700

    updated webpack with hot swapping

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './public/js/app.js',
+  devServer:{
+    contentBase: __dirname + '/dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js',
